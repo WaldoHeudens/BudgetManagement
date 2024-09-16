70f6ffd9d65d3b15abb6c198bc2b139aa764a654
Waldo <Waldo@DESKTOP-34N2AV9>
2024-09-16 22:41:45 +0200

    Add project files.

---
+++ BudgetManagement_C/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetManagement_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
